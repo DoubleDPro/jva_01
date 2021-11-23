f8270d815e4f472ee0c45451a508a754dd1c98f6
Усачев Дмитрий Дмитриевич <92usachevdd@region.cbr.ru>
2021-11-23 14:01:51 +0300

    #exercise_2
    Выполнен рефакторинг FileSystem под шаблон Composite

--- src/main/java/exercise_2/Link.java
+++ src/main/java/exercise_2/Link.java
@@ -2,18 +2,11 @@ package exercise_2;
 
 public class Link extends FileSystemComposite {
 
-  private String name;
-  private String path;
   private FSItem link;
-  private FSItem parent;
 
   public Link(String name, String path, FSItem link) {
     super(name, path);
     this.link = link;
-  }
-
-  public void addChild(FileSystemComposite fileSystemComposite) {
-    super.add(fileSystemComposite);
   }
 
   public FSItem getLink() {
